dcf7e868b62b931ed09b816b3cadad963cb68323
Matthew Feng <mattfeng@mit.edu>
2022-08-03 08:35:33 -0400

    more pages to make

---
+++ src/pages/team.js
@@ -0,0 +1,19 @@
+import * as React from "react";
+import Layout from "../components/Layout";
+
+const TeamPage = () => {
+  return (
+    <Layout>
+      <main>
+        <h1>Our 2022 iGEM Team!</h1>
+      </main>
+    </Layout>
+  );
+};
+
+export default TeamPage;
+
+// Include pictures of your teammates, don't forget instructors and advisors!
+// You can add a small biography or a few words from each team member, to tell us what you like, and what motivated you to participate in iGEM.
+// Take team pictures! Show us your school, your lab and little bit of your city.
+// Remember that image galleries can help you showcase many pictures while saving space.
